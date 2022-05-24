c3e7f69ef38f02b91578ee7d5eae531baa91701c
Miguel Ajila <maajila1@espe.edu.ec>
2022-05-23 21:39:25 -0500

    Colocamos variables y input para que el usuario interactue.

--- DuInteligente.py
+++ DuInteligente.py
@@ -1,5 +1,11 @@
 print ("Ducha Inteligente")
 def DuchaElec():
-
+    #Habtaciones es la variable donde se encuentra el entorno de la ducha
+    Habitaciones = {'Sala': '0', 'Matrimonial': '0', 'Hospedaje': '0'}
+    Habitacion = input("Habitacion donde de encuentra \nSala, Matrimonial, Hospedaje \n")
+    Temperatura = float(input("Ingrese la temperatura del agua: "))
+    #Como sabemos la temperatura es un elemento flotante.
+    #Considerando el ambiente de Santo Domingo la temperatura normal es de 23 grados
+    grados = 0.23
 
     DuchaElec()
